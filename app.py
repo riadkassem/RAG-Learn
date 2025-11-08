e7c44e98d15995e1f6923dfabd9deeb86c9e3608
Riad <riad.kassem17@gmail.com>
2025-11-08 22:52:54 +0200

    Fixed langchain_text_splitters import

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
